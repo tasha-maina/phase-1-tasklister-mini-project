bb74ec29d87a46b0bb8e8e83269601c214908016
tasha maina <tashabanks271@gmail.com>
2025-07-02 20:27:57 +0300

    Add basic task creation functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.getElementById("create-task-form");
+  form.addEventListener("submit", function(event) {
+    event.preventDefault(); 
+    let taskInput = document.getElementById("new-task-description");
+    let taskText = taskInput.value;
+
+    let li = document.createElement("li");
+    li.textContent = taskText;
+
+    let taskList = document.getElementById("tasks");
+    taskList.appendChild(li);
+
+    taskInput.value = "";
+  });
 });
+
